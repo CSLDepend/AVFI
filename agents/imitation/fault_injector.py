88337ce85db69d0718349d7571c7b3e8c5484f19
jcyriac2 <jcyriac2@illinois.edu>
2018-03-19 16:05:59 -0500

    Dump Camera Output to Video File

--- agents/imitation/fault_injector.py
+++ agents/imitation/fault_injector.py
@@ -1,19 +1,29 @@
 import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import numpy as np
+import atexit
+import copy
+#Update this path to point to ffmpeg.exe
+plt.rcParams['animation.ffmpeg_path'] = "E:\\ffmpeg\\bin\\ffmpeg.exe"
+#Matplotlib animation
+ims=[]
+fig = plt.figure()
+
+def saveVideo():
+    ani = animation.ArtistAnimation(fig,ims, interval = 50)
+    mywriter = animation.FFMpegWriter(fps=20)
+    ani.save('test.mp4',writer=mywriter)
+
+atexit.register(saveVideo)
 
 class FaultInjector(object):
     def __init__(self,input_fm_in):
-        self.camera_img=None
         self.input_fm = input_fm_in
-        plt.ion()
 
     def corruptControls(self,control_data):
         return control_data
 
     def corruptSensors(self,sensor_data):
         sensor_data['CameraRGB']._converted_data=self.input_fm.inject(sensor_data['CameraRGB'].data)
-        if(self.camera_img == None):
-            self.camera_img = plt.imshow(sensor_data['CameraRGB'].data)
-        self.camera_img.set_data(sensor_data['CameraRGB'].data)
-        plt.draw()
-        plt.pause(0.01)
+        ims.append([plt.imshow(sensor_data['CameraRGB'].data,animated=True)])
         return sensor_data
